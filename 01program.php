66def774e7d2a3a3c5988f3610dea6bf581229ba
PatrykOsz <66477051+PatrykOsz@users.noreply.github.com>
2020-06-05 15:18:49 +0200

    Piąty commit

--- 01program.php
+++ 01program.php
@@ -3,5 +3,6 @@ echo ("Witaj");
 echo ("<br> Jestem programem 01");
 echo ("<br> Test11111");
 echo ("<br> Test22222");
-echo ("<br> Dalsza czesc programu")
+echo ("<br> Dalsza czesc programu");
+echo ("<br> Dalsza czesc programu 22222 z GitHuba");
 ?>
